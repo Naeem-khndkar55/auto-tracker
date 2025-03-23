5f00a2cd17bf49c1f5b44bffec646e7e844e4e73
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-03-23 17:15:48 +0600

    vite.config and unnecessery file deleted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,16 +7,16 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Backend port
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    host: true,
-    port: 5173,
-    historyApiFallback: true,
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:5000', // Backend port
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  //   host: true,
+  //   port: 5173,
+  //   historyApiFallback: true,
+  // },
 });
