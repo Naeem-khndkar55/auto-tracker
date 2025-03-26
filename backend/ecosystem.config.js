d2006a0a4ead63eb25e55fa9952dccd5c2942e01
Ahashan Alam Sojib <ahashans@gmail.com>
2025-03-27 00:18:49 +0600

    Refactor ecosystem configuration by removing development environment settings

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,14 +3,9 @@ module.exports = {
     name: "cumillavehicle-backend",
     script: "./server.js",
     env: {
-        NODE_ENV: "development",
-        PORT: 5000,
-        BASE_URL: "http://localhost:5000"
-    },  
-    env_production: {
       NODE_ENV: "production",
-      PORT: 5000,      
-    },
+      PORT: 5000
+    },     
     instances: 1,
     autorestart: true,
     watch: false,
